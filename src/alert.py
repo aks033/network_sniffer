eea174954cfa8fdb069365e5e692c9a9064c80cf
aks033 <akankshasharma1792@gmail.com>
2019-01-19 12:35:18 -0500

    added logging

--- src/alert.py
+++ src/alert.py
@@ -2,6 +2,11 @@ from threading import Thread
 import time
 import operator
 from datetime import datetime, timedelta
+import logging
+import os
+import config
+import os 
+
 
 class Alert(Thread):
 	''' Alert mechanism for the application '''
@@ -16,6 +21,8 @@ class Alert(Thread):
 		self.alert_time = None
 		self.traffic_at_alert_time = None
 		self.alert_recovery_time = None
+		#path = os.path.dirname(os.getcwd())
+		#logging.basicConfig(filename = path+config.alert_file, level=logging.DEBUG)
 
 	def run(self):
 		self.examine_traffic_behaviour()
@@ -23,27 +30,34 @@ class Alert(Thread):
 	def examine_traffic_behaviour(self):
 
 		while(True):
-			# last packet received
-			time_of_last_hit = self.sniffer_obj.traffic_queue[-1:] 
-			start_time_of_traffic_eval = datetime.now() - timedelta(seconds=120)
+			try:
+				# last packet received
+				time_of_last_hit = self.sniffer_obj.traffic_queue[-1:] 
+				start_time_of_traffic_eval = datetime.now() - timedelta(seconds=120)
 
-			# store in taffic queue only urls hit during the last 2 minutes
-			self.sniffer_obj.traffic_queue = [i for i in self.sniffer_obj.traffic_queue if i[0] >= start_time_of_traffic_eval]
-			#no. of hits in last two minutes
-			traffic_last_two_mins = len(self.sniffer_obj.traffic_queue)
-			#print(self.sniffer_obj.traffic_queue)
+				# store in taffic queue only urls hit during the last 2 minutes
+				self.sniffer_obj.traffic_queue = [i for i in self.sniffer_obj.traffic_queue if i[0] >= start_time_of_traffic_eval]
+				
+				#no. of hits in last two minutes
+				traffic_last_two_mins = len(self.sniffer_obj.traffic_queue)
+				
+				#print(self.sniffer_obj.traffic_queue)
 
-			if(traffic_last_two_mins > self.avg_hits):
-				#self.generate_high_traffic_alert(traffic_last_two_mins, time_of_last_hit[0])
-				self.high_traffic_flag = True
-				self.recovered_flag = False
-				self.alert_time =  time_of_last_hit[0][0]
-				self.traffic_at_alert_time = traffic_last_two_mins
+				if(traffic_last_two_mins > self.avg_hits and time_of_last_hit[0][0] != self.alert_time):
+					#self.generate_high_traffic_alert(traffic_last_two_mins, time_of_last_hit[0])
+					self.high_traffic_flag = True
+					self.recovered_flag = False
+					self.alert_time =  time_of_last_hit[0][0]
+					self.traffic_at_alert_time = traffic_last_two_mins
+					logging.info("\n*********** HIGH TRAFFIC GENERATED AN ALERT : URL HITS - {} AT TIME {} ***********".format(self.traffic_at_alert_time, self.alert_time))
 
-			elif (self.high_traffic_flag and traffic_last_two_mins < self.avg_hits):
-				self.high_traffic_flag = False
-				#self.generate_recovered_high_traffic_alert(time_of_last_hit[0])
-				self.alert_recovery_time = datetime.now()
-				self.recovered_flag = True
-			time.sleep(30)
+				elif (self.high_traffic_flag and traffic_last_two_mins < self.avg_hits):
+					self.high_traffic_flag = False
+					#self.generate_recovered_high_traffic_alert(time_of_last_hit[0])
+					self.alert_recovery_time = datetime.now()
+					self.recovered_flag = True
+					logging.info("\n********** RECOVERED FROM HIGH TRAFFIC AT {} ************".format(self.alert_recovery_time))
+				time.sleep(10)
+			except Exception as e:
+				print str(e)
 
