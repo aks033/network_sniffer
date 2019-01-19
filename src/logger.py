5c38f8c2584e5d71c32d7c363e92a652556fd953
aks033 <akankshasharma1792@gmail.com>
2019-01-19 15:15:52 -0500

    removed logger.py

--- src/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-class TrafficLog:
-	def __init__ (self, file_name):
-		logging.basicConfig(filename = file_name, level=logging.INFO)
-
-
-	def log_info(msg_str):
-		logging.info(msg_str)
